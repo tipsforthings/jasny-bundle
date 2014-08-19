96987d1cd35d2087af3f33b8b246a4463f1e40ad
Alex Scott <alex@tipsforthings.com>
2014-08-19 13:18:10 +0100

    Fix font-awesome precompile

--- lib/jasny_bundle.rb
+++ lib/jasny_bundle.rb
@@ -6,7 +6,7 @@ module JasnyBundle
     if defined?(::Rails) and Gem::Requirement.new('>= 3.2').satisfied_by?(Gem::Version.new ::Rails.version)
       class Rails::Engine < ::Rails::Engine
         # this class enables the injection of files into the asset pipeline
-      end
+        config.assets.precompile += %w(*.woff *.ttf *.eot *.svg)      end
     end
   end
 end
